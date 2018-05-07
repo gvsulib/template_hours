b29ed81aa7c061110abdec0c94685f3f1fcf1e62
Matthew Reidsma <reidsmam@gvsu.edu>
2018-05-07 11:12:33 -0400

    Updated path to cron output

--- hours.js
+++ hours.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
 	console.log('Loading today\'s hours...');
-	$('.site .library-hours').load('https://matthew.reidsrow.com/temp/hours.php');			
+	$('.site .library-hours').load('https://prod.library.gvsu.edu/labs/template_hours/hours.php');			
 });
